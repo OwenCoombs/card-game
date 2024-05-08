57bee2933cf6a48c6848fc2c63ceefec5c8d8373
Owen Coombs <owencoombs03@gmail.com>
2024-05-08 16:52:34 -0400

    almost mvp

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
+import random
 
-class Cards:
-    def __init__(self, rank, suit):
-        self.rank = rank
+
+
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
         self.suit = suit
 
     def display(self):
-        print(f'{self.rank} of {self.suit}')
-        
-your_cards = Cards(3, 'heart')
-your_cards.display()
+        print(f'{self.suit} of {self.value}')
+
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for s in ["Spades", "Clubs", "Diamonds","Hearts"]:
+            for v in range(1,14):
+                self.cards.append(Card(str(v), s))
+                # print ("{} of {}".format(v, s))
+
+    def show(self):
+        for cards in self.cards:
+            cards.display()
+
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    
+    def drawCard(self):
+        return self.cards.pop(0)
+    
+
+class players:
+    def __init__(self):
+        self.hand = []
+
+    def draw (self, deck):
+        self.hand.append(deck.drawCard()) 
+
+
+
+
+
+
+deck = Deck()
+deck.shuffle()
+card = deck.drawCard()
+card.display()
+
+
+
+
+
+
+
